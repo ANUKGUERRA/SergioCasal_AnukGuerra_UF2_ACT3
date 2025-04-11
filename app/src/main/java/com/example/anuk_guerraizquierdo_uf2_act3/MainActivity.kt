f65b64ab0dff6e690e9d5533dc3d275eef3235c8
A21sercasger <146182037+A21sercasger@users.noreply.github.com>
2025-04-11 16:12:50 +0200

    Sounds and sprite size

--- app/src/main/java/com/example/anuk_guerraizquierdo_uf2_act3/MainActivity.kt
+++ app/src/main/java/com/example/anuk_guerraizquierdo_uf2_act3/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.anuk_guerraizquierdo_uf2_act3
 
 import android.content.Intent
 import android.graphics.drawable.AnimationDrawable
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -10,14 +11,26 @@ import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private var attack1Sound: MediaPlayer? = null
+    private var attack2Sound: MediaPlayer? = null
+    private var backgroundMusic: MediaPlayer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        backgroundMusic = MediaPlayer.create(this, R.raw.background_music)
+        backgroundMusic?.isLooping = true
+        backgroundMusic?.setVolume(0.1f, 0.1f) // Lower volume for background music
+        backgroundMusic?.start()
 
         val characterImageView = findViewById<ImageView>(R.id.characterImageView)
         val btnAttack1 = findViewById<Button>(R.id.btnAttack1)
         val btnAttack2 = findViewById<Button>(R.id.btnAttack2)
         val btnNextActivity = findViewById<Button>(R.id.btn3)
+
+        attack1Sound = MediaPlayer.create(this, R.raw.attack1)
+        attack2Sound = MediaPlayer.create(this, R.raw.attack2)
 
         // Setup idle animation
         fun setIdleAnimation() {
@@ -31,6 +44,7 @@ class MainActivity : AppCompatActivity() {
 
         // Attack 1 button
         btnAttack1.setOnClickListener {
+            attack1Sound?.start()
             characterImageView.setBackgroundResource(R.drawable.ataque1)
             val attackAnimation = characterImageView.background as AnimationDrawable
             attackAnimation.start()
@@ -48,6 +62,7 @@ class MainActivity : AppCompatActivity() {
 
         // Attack 2 button
         btnAttack2.setOnClickListener {
+            attack2Sound?.start()
             characterImageView.setBackgroundResource(R.drawable.ataque2)
             val attackAnimation = characterImageView.background as AnimationDrawable
             attackAnimation.start()
@@ -68,5 +83,12 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, SecondActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        attack1Sound?.release()
+        attack2Sound?.release()
+        backgroundMusic?.release()
     }
 }
